d1c8b5098b376d39cdea7cc39c168c7b9e7cb259
Tomas <tomas@users.github.com>
2017-10-27 14:42:43 +0100

    kxzfjh

--- old.py
+++ old.py
@@ -8,5 +8,5 @@ while 1:
     total = 0
     for num in line.split(" "):
         total += int(num)
-    print("Total: %d (%f%%)" % (total, total * (100.0 / out_of)))
+    print("ksdjgk Total: %d (%f%%)" % (total, total * (100.0 / out_of)))
 
