ee27ef04b43b74fe6c04a53ddcaa21bfad058a5f
Tomas <tomas@users.github.com>
2017-10-27 15:25:47 +0100

    Int changed to float

--- old.py
+++ old.py
@@ -7,6 +7,6 @@ while 1:
     line = input()
     total = 0
     for num in line.split(" "):
-        total += int(num)
+        total += float(num)
     print("Cool Total: %d (%f%%)" % (total, total * (100.0 / out_of)))
 
